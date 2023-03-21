933873afff8b45a8c5696925e603cbedf0f44ad0
qtsun <838280705@qq.com>
2023-03-21 11:39:49 +0800

    3-12 自定义hook 第三部分 - 正确的方式完成URLLoader

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import Hello from './components/Hello'
-import MouseTracker from './components/MouseTracker';
+// import MouseTracker from './components/MouseTracker';
 import LikeButton from './components/LikeButton';
 import useMousePosition from './hooks/useMousePosition';
+import useURLLoader from './hooks/useURLLoader';
 import './App.css';
+
+interface IShowResult {
+message:string,
+status:string
+}
 
 const App: React.FC = () => {
   const [show, setShow] = useState(true)
   const positions = useMousePosition()
+  const [ data , loading ] = useURLLoader('https://dog.ceo/api/breeds/image/random', [show])
+  const dogResult = data as IShowResult
   return (
     <div className="App">
       <header className="App-header">
@@ -18,7 +26,7 @@ const App: React.FC = () => {
         </p>
         <Hello />
         <p>X: {positions.x}, Y: {positions.y}</p>
-
+        {loading ? <p>🐕 读取中</p> : <img src={dogResult && dogResult.message} alt=""/>}
         <LikeButton />
         {/* {show && <MouseTracker />} */}
         <a
